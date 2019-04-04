6bb102adbe27342915338ff91d5857c71da902f8
kmietotwy <kmietotwy@gmail.com>
2019-04-04 10:23:14 -1000

    currently only gets all prime factors

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -8,7 +8,44 @@
  */
 module.exports = function( ceiling ) {
   // do work here
+    
+  var allPrime = [];
+  let LCM = 0;
 
+  function findPrimeFactors (num) {
+    while (num % 2 === 0) {
+        allPrime.push(2);
+        num = num / 2;
+    }
+  
+    var sqrtNum = Math.sqrt(num);
+    for (var i = 3; i <= sqrtNum; i++) {
+        while (num % i === 0) {
+            primeFactors.push(i);
+            num = num / i;
+        }
+    }
+
+    if (num > 2) {
+        allPrime.push(num);
+    }
+  }
+  
+  for(let a=2; a<x+1; a++){
+    findPrimeFactors(a);
+  }
+  
+  allPrime.sort();
+
+  //not yet done!!
+  for(let b=0; b<allPrime.length; b++){
+   
+  }
+
+ let currentNum = 0;
+ let instances = 0;
+  
+  console.log(allPrime);
 
   return 0;
 };
